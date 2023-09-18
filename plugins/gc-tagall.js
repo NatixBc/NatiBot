af42e426d9a380122de900782deab5b5976df9a6
NatixBc <144946924+NatixBc@users.noreply.github.com>
2023-09-18 11:41:14 -0500

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,10 +5,10 @@ throw false
 }
 let pesan = args.join` `
 let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let teks = `*⺀𝑁𝐴𝑇𝐼𝐵𝑂𝑇 𝐿𝑂𝑆 𝐼𝑁𝑉𝑂𝐶𝐴 𝐷𝐸𝑆𝑃𝐼𝐸𝑅𝑇𝐸𝑁 𝑃𝐿𝐴𝑁𝑇𝐴𝑆⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `┣👻 @${mem.id.split('@')[0]}\n`}
-teks += `└𝐁𝐂`
+teks += `♥_🤍 @${mem.id.split('@')[0]}\n`}
+teks += `└Natibot ≧◠ᴥ◠≦✊` 
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
